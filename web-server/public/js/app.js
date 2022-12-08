cdf3471d371d0bc11348d59cc854bae17f1ae26a
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-12-08 13:11:33 -0600

    Wire up ui to have a form to search for weather forecast

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,16 +2,22 @@ console.log('Client side javascript file is loaded');
 
 const weatherForm = document.getElementById('weather-form');
 const searchInput = document.getElementById('search-input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
 
 weatherForm.onsubmit = (e) => {
     e.preventDefault();
     const location = searchInput.value;
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
     fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
         if (data.error) {
-            console.error(data.error);
+            messageOne.textContent = data.error;
+            messageTwo.textContent = '';
         } else {
-            console.log(data);
+            messageOne.textContent = 'Temperature is ' + data.temperature + ' degrees Celsius. It feels like ' + data.tempSensation + ' degrees.';
+            messageTwo.textContent = 'The forecast for today is ' + data.weatherDescription + '.';
         }
     });
 });
